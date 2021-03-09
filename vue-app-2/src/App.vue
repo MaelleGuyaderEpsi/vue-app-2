dd6310975f5ff9000df9a0b601c4d058fb966a0c
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-09 18:35:26 +0100

    last the last commit

--- vue-app-2/src/App.vue
+++ vue-app-2/src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+//API METEO MATICS: https://www.meteomatics.com/en/weather-api/?msclkid=82611baf1cf31b251b4c7be8b6666c3a - attente de la validation de l'inscription
+//API METEO CONCEPT: https://api.meteo-concept.com/documentation
 import home from './components/home.vue'
 
 export default {
