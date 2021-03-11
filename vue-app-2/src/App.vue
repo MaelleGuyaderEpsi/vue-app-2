36d3a2810a7765c81d29f3ca3bf28a2daecd4155
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-11 19:03:25 +0100

    commit 11/03

--- vue-app-2/src/App.vue
+++ vue-app-2/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo-meteo.png">
-  <home msg="Météo par département"/>
+  <home msg="Météo par ville"/>
   
 </template>
 
 <script>
-//API METEO MATICS: https://www.meteomatics.com/en/weather-api/?msclkid=82611baf1cf31b251b4c7be8b6666c3a - attente de la validation de l'inscription
-//API METEO CONCEPT: https://api.meteo-concept.com/documentation
+//API METEO MATICS: https://www.meteomatics.com/en/weather-api/?msclkid=82611baf1cf31b251b4c7be8b6666c3a - attente de la validation de l'inscription -> Nombre de requ^^etes très limités
+//API METEO CONCEPT: https://api.meteo-concept.com/documentation -> indispo a cause de l'incendie OVH
+//API PRISE -> https://openweathermap.org/current -> nb requêtes 
+
 import home from './components/home.vue'
 
 export default {
@@ -22,8 +24,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
+  color: #109CD1;
   margin-top: 60px;
 }
 </style>
--- vue-app-2/src/components/home.vue
+++ vue-app-2/src/components/home.vue
@@ -1,8 +1,11 @@
 <template>
+  
 
   <div class="home">
+
     <h1>{{ msg }}</h1>
-    <p><button class="button button1" @click="meteo">Voir la météo</button></p>
+    <p><input type="number" name="name" id="name" v-model="name" placeholder="code postal"></p>
+    <p><button class="button button1" @click="meteo" type="submit">Voir la météo</button></p>
   </div>
 </template>
 
@@ -32,9 +35,9 @@ a {
   color: #42b983;
 }
 .button {
-  background-color: #109CD1; /* Green */
+  background-color: #109CD1; 
   border: none;
-  color: white;
+  color: #109CD1;
   padding: 16px 32px;
   text-align: center;
   text-decoration: none;
@@ -46,12 +49,32 @@ a {
 }
 .button1 {
   background-color: white; 
-  color: black; 
+  color: #109CD1; 
   border: 2px solid #109CD1;
 }
 
 .button1:hover {
   background-color: #109CD1;
   color: white;
+}
+
+.button1:focus {
+  outline: none;
+  background-color: #109CD1;
+  color: white;
+}
+
+input {
+  background-color: white; 
+  border: solid 2px #109CD1;
+  color: #109CD1;
+  padding: 10px 5px;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+}
+
+input:focus {
+  outline: none;
 }
 </style>
