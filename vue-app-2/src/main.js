804741234ccff19f1f3ce673efa8c4fb1d80a76f
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-15 19:56:41 +0100

    commit 15/03

--- vue-app-2/src/main.js
+++ vue-app-2/src/main.js
@@ -1,5 +1,9 @@
 import{ createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 //BOOTSTRAP***
 //import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
@@ -20,3 +24,5 @@ new Vue({
     router,
     render: h => h(App)
 }).$mount('#app')*/
+
+Vue.use(VueAxios, axios)
--- vue-app-2/src/views/home.vue
+++ vue-app-2/src/views/home.vue
@@ -4,20 +4,44 @@
     <h1>{{ msg }}</h1>
     <p><input type="number" name="name" id="name" v-model="codePostal" placeholder="code postal"></p>
     <p>Le code postal choisi est le: {{ codePostal}} </p>
-    <p><button class="button button1" @click="lerouteur" method="post">Voir la météo</button></p>
+
+
+    <p><button type="submit" class="button button1" @click="lerouteur" method="post">Voir la météo</button></p>
+
     
+  </div>
+
+  <div class="divDroite">
+    <p>Température: {{ temp }} Klein</p>
+    <p>Ville: {{ name }}</p>
+    
+    <p>http://api.openweathermap.org/data/2.5/weather?zip={{codePostal}},fr&appid=19737a5f3ac229742634fc3eeb00355f</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'home',
+  data() {
+    return {
+        codePostal: "",
+        temp : ""
+        }
+  },
   methods: {
     lerouteur: function() {
       this.$router.push({name: 'infos', params: {username: 'informations'}
       })
-    }
+    },
+     getAPI () {
+    axios
+    .get("http://api.openweathermap.org/data/2.5/weather?zip={{codePostal}},fr&appid=19737a5f3ac229742634fc3eeb00355f")
+    .then(response => (this.temp = response))
   }
+  }
+ 
 }
 </script>
 
@@ -59,5 +83,13 @@ input:focus {
 .button1:hover {
   background-color: #109CD1;
   color: white;
+}
+
+.divDroite {
+  background-color: white;
+  text-align: center;
+  padding: 10px 5px;
+  left: 500px;
+  right: 0px;
 }
 </style>
